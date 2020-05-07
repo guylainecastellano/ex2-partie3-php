089f7cfc335e81f81dd6f9d9aabe4d1d5c86ea3a
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-07 14:59:17 +0200

    ex2 partie3 php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$months2 = array();   //2e version
+$months2[0] = 'janvier';
+$months2[1] = 'fevrier';
+$months2[2] = 'mars';
+$months2[3] = 'avril';
+$months2[4] = 'mai';
+$months2[5] = 'juin';
+$months2[6] = 'juillet';
+$months2[7] = 'aout';
+$months2[8] = 'septembre';
+$months2[9] = 'octobre';
+$months2[10] = 'novembre';
+$months2[11] = 'decembre';
+  ?>
+    <!DOCTYPE html>
+  <html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>ex2 partie3 php</title>
+  </head>
+  <body>
+    <?php echo $months2[5] ?>
+  </body>
+  </html>
